56be4089c17eb87313c3b967629b3b930a0bd5f6
BlueberryWeb <blueberryweb7@gmail.com>
2022-04-20 17:08:24 -0500

    cambios

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menu(){
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('showcase').style.display = 'none';
+}
+function cerrar(){
+    document.getElementById('showcase').style.display = 'flex';
+    document.getElementById('menu').style.display = 'none';
+}
